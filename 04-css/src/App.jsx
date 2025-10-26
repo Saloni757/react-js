c11d34cc4b59f71044cb28b0663a8270654f9670
Saloni757 <sahusaloni458@gmail.com>
2025-10-26 08:56:44 +0530

    CSS

---
+++ 04-css/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './component/header/Header.jsx'
+import Button from './component/button/Button.jsx'
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Button />
+    </div>
+  )
+
+}
+
+export default App
