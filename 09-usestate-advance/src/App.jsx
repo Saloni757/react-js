d151022d6de1b3fe13449975ac34e33c2cab9c11
Saloni757 <sahusaloni458@gmail.com>
2025-11-18 08:08:35 +0530

    advance usestate

---
+++ 09-usestate-advance/src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+const App = () => {
+
+  const [num, setNum] = useState([10, 20, 30])
+
+  const btnClicked = () => {
+    const newNum = [...num]
+    newNum[3] = 40
+    setNum(newNum)
+
+
+  }
+
+  return (
+    <div>
+      <h1>{num}</h1>
+      <button onClick={btnClicked}>click</button>
+    </div>
+  )
+}
+
+export default App
