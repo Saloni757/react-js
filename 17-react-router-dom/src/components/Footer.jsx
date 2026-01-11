b6224ecb39175c4a0b2ef8c25693b1316df2e9f7
Saloni757 <sahusaloni458@gmail.com>
2026-01-11 22:50:39 +0530

    Routes

---
+++ 17-react-router-dom/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="fixed bottom-0 w-full flex justify-center items-center py-4 bg-cyan-900">
+      <h3>This is footer</h3>
+    </div>
+  )
+}
+
+export default Footer
